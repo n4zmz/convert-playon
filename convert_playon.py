e3f6d2a4b91f93f126e7dd4459fdee4e3c852f29
Ubuntu <ubuntu@pi4-mediaserver.B-lan.com>
2020-10-28 22:20:10 +0000

    Daily update

--- convert_playon.py
+++ convert_playon.py
@@ -5,11 +5,26 @@ import os
 import os.path
 import subprocess
 import re
+import json
 
 inputFolder  = '/tank/nfsshare/Videos/PlayOn'
 outputFolder = '/tank/nfsshare/Videos/PlayOn-trimmed'
 startSkipSeconds = 5
 endSkipSeconds = 6
+
+def single_chapter (full_fn,fn,duration):
+    print('Single Chapter extract:',fn,duration)
+    process = subprocess.Popen(['ffprobe','-v','0','-show_entries','format=duration','-of','compact=p=0:nk=1',full_fn],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = process.communicate()
+    out.replace('\n','')
+    dur = float(out)
+    tot_dur = dur - startSkipSeconds - endSkipSeconds
+    print('Original Dur',dur,'New Dur',tot_dur)
+    dir_fn = outputFolder + full_fn[len(inputFolder):]
+    exists = os.path.exists(dir_fn)
+    if not exists:
+        print('process')
+    return
 
 def main ( argv = None):
     o_exists = os.path.exists(outputFolder)
@@ -19,20 +34,23 @@ def main ( argv = None):
         os.makedirs(outputFolder)
 
     for root, dirs, files in os.walk(inputFolder):
-        print('root',root,'dirs',dirs,'files',files)
+#        print('root',root,'dirs',dirs,'files',files)
         destdir = outputFolder + root[len(inputFolder):]
         exists = os.path.exists(destdir)
         if not exists:
             os.makedirs(destdir)
         for file in files:
             full_fn = os.path.join(root,file)
-#            full_fn = full_fn.replace(' ','\\ ')
             print full_fn
-            import pdb;pdb.set_trace()
             process = subprocess.Popen(['ffprobe',full_fn,'-show_chapters','-print_format','json'],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = process.communicate()
-            print('stdout',out)
-            print('stderr',err)
+            myjson = json.loads(out)
+            chap = myjson['chapters']
+            m = re.search('Duration: (\d{2}:\d{2}\:\d{2}.\d+)',err)
+            if len(chap) == 0:
+                single_chapter(full_fn,file,m.group(1))
+            else:
+                print('Chap',chap)
 if __name__ == '__main__':
     main()
     exit(0)
